3251fb11f5751b5169b9e118381b985a4d7feb76
walid <s3972717@student.rmit.edu.au>
2024-05-07 12:25:30 +1000

    .

---
+++ ftt.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "LinkedList.h"
+
+/**
+ * manages the running of the program, initialises data structures, loads
+ * data, display the main menu, and handles the processing of options. 
+ * Make sure free memory and close all files before exiting the program.
+ **/
+
+// Function to handle option 1: Display Meal Options
+void displayMealOptions() {
+    // Implement logic to display meal options
+    std::cout << "Meal options are displayed here.\n";
+}
+
+
+int main(int argc, char **argv)
+{
+    /* validate command line arguments */
+    // TODO
+
+    std::cout << "Just a test, nothing implemented yet!" << std::endl;
+    std::cout << "Main Menu" << std::endl;
+    std::cout << "    1. Display Meal Options" << std::endl;
+    std::cout << "    2. Purchase Meal" << std::endl;
+    std::cout << "    3. Save and Exit" << std::endl;
+    std::cout << "Administrator-Only Menu:" << std::endl;
+    std::cout << "    4. Add Food" << std::endl;
+    std::cout << "    5. Remove Food" << std::endl;
+    std::cout << "    6. Display Balance" << std::endl;
+    std::cout << "    7. Display Balance" << std::endl;
+std::cout << "Select your option (1-7) :"<<std::endl;
+
+    while 
+    return EXIT_SUCCESS;
+}
