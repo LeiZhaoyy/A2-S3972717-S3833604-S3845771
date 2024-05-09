3129c6f212f317d79e8d957fd1356433fa468527
LeiZhaoyy <s3833604@student.rmit.edu.au>
2024-05-10 01:16:13 +1000

    Modified to utilize the modulized Helper function to fulfill REQ 3

---
+++ Helper.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <sstream>
+#include "Helper.h"
+
+using namespace std;
+
+void Helper::printMainMenu(bool isAdmin)
+{
+	std::cout << "Main Menu" << std::endl;
+	std::cout << "    1. Display Meal Options" << std::endl;
+	std::cout << "    2. Purchase Meal" << std::endl;
+	std::cout << "    3. Save and Exit" << std::endl;
+	
+	if (isAdmin)
+	{
+		std::cout << "Administrator-Only Menu:" << std::endl;
+		std::cout << "    4. Add Food" << std::endl;
+		std::cout << "    5. Remove Food" << std::endl;
+		std::cout << "    6. Display Balance" << std::endl;
+		std::cout << "    7. Display Balance" << std::endl;
+	}
+}
+
+int Helper::requestMenuSelection(bool isAdmin)
+{
+	if (isAdmin)
+	{
+		cout << "Select your option (1-7): ";
+	}
+	else
+	{
+		cout << "Select your option (1-3): ";
+	}
+
+	string input;
+	getline(cin, input);
+
+	if (input == "Exit")
+		return 3;
+
+	if (isNumber(input))
+		return stoi(input);
+	else
+		return -1;
+}
+
+bool Helper::isNumber(string s)
+{
+	string::const_iterator it = s.begin();
+	char dot = '.';
+	int nb_dots = 0;
+	while (it != s.end())
+	{
+		if (*it == dot)
+		{
+			nb_dots++;
+			if (nb_dots > 1)
+			{
+				break;
+			}
+		}
+		else if (!isdigit(*it))
+		{
+			break;
+		}
+
+		++it;
+	}
+	return !s.empty() && s[0] != dot && it == s.end();
+}
--- ftt.cpp
+++ ftt.cpp
@@ -1,66 +1,51 @@
 #include <iostream>
 #include "LinkedList.h"
+#include "Helper.h"
 
-/**
- * manages the running of the program, initialises data structures, loads
- * data, display the main menu, and handles the processing of options. 
- * Make sure free memory and close all files before exiting the program.
- **/
-
-
-
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    /* validate command line arguments */
-    bool running = true;
-    while (running){ 
+	//if (argc < 3)
+	//{
+	//	std::cerr << "Usage: " << argv[0] << " <foodsfile> <coinsfile>" << std::endl;
+	//	return EXIT_FAILURE;
+	//}
 
- if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " <foodsfile> <coinsfile>" << std::endl;
-        running = false;
-    }
+	//// Extract filenames from command line arguments
+	//std::string foodsFile = argv[1];
+	//std::string coinsFile = argv[2];
 
-    // Extract filenames from command line arguments
-    std::string foodsFile = argv[1];
-    std::string coinsFile = argv[2];
+	//// Create an instance of the LinkedList class
+	//LinkedList foodList;
 
-    // Create an instance of the LinkedList class
-    LinkedList foodList;
+	//// Load food data from the specified file
+	//foodList.loadFromFile(foodsFile);
 
-    // Load food data from the specified file
-    foodList.loadFromFile(foodsFile);
+	while (true)
+	{
+		Helper::printMainMenu();
 
-    // Now foodList contains the loaded food data, you can use it to display the menu or perform other operations
+		switch (Helper::requestMenuSelection())
+		{
+			case 1:
+				break;
 
+			case 2:
+				break;
 
+			case 4:
+				break;
 
+			case 5:
+				break;
 
+			case 6:
+				break;
 
+			case 7:
+				break;
 
-    std::cout << "Main Menu" << std::endl;
-    std::cout << "    1. Display Meal Options" << std::endl;
-    std::cout << "    2. Purchase Meal" << std::endl;
-    std::cout << "    3. Save and Exit" << std::endl;
-    std::cout << "Administrator-Only Menu:" << std::endl;
-    std::cout << "    4. Add Food" << std::endl;
-    std::cout << "    5. Remove Food" << std::endl;
-    std::cout << "    6. Display Balance" << std::endl;
-    std::cout << "    7. Display Balance" << std::endl;
-std::cout << "Select your option (1-7) :"<<std::endl;
-            int choice;
-
-            std::cin >> choice;
-
-
-    if (choice == 1){
-            foodList.displayMenu();
-
-    }
-    else{
-        running = false;
-    }
-    }
-   
-    
-    return EXIT_SUCCESS;
+			default:
+				return EXIT_SUCCESS;
+		}
+	}
 }
